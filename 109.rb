57ab73873a4ec6f94eb5f908704ae80a1dc72a96
rk0der <ryo.alter@gmail.com>
2016-09-30 19:36:39 +0900

    refactor 1-09

--- 109.rb
+++ 109.rb
@@ -1,11 +1,5 @@
 def typoglycemia(s)
-  s.split.map do |w|
-    if w.length > 4
-      w[0] + w[1..-2].chars.shuffle.join + w[-1]
-    else
-      w
-    end
-  end.join(' ')
+  compress = lambda{|w| w[0] + w[1..-2].chars.shuffle.join + w[-1]}
+  s.split.map{|w| if w.length > 4 then compress[w] else w end}.join(' ')
 end
-
 puts typoglycemia(ARGV[0])
