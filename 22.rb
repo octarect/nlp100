1836511b3126680123cfc8f6fb6640cab5920348
rk0der <ryo.alter@gmail.com>
2017-04-06 23:41:25 +0900

    solve 22

---
+++ 22.rb
@@ -0,0 +1,38 @@
+require "json"
+
+class String
+  def match_all(reg)
+    ret = []
+    str = self
+
+    until str.nil?
+      m = str.match(reg)
+      unless m.nil?
+        for i in 1..(m.length-1)
+          ret.push m[i]
+        end
+        str = m.post_match
+      else
+        break
+      end
+    end
+    ret
+  end
+end
+
+# Constant value
+key_title = "title"
+key_article = "text"
+
+# Usage: command [filename] [title]
+fname = ARGV[0]
+
+data = []
+File.foreach(fname) do |line|
+  d = JSON.parse(line)
+  cs = d[key_article].match_all(/\[\[Category:(.*)\]\]/)
+  unless cs.empty?
+    puts "#{d[key_title]}:"
+    puts "\t#{cs.join(', ')}"
+  end
+end
